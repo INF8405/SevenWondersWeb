f50f7d42da233b347ed05eb9ff367718fe161188
Guillaume Masse <masgui@localhost.localdomain>
2014-02-10 19:48:43 -0500

    css to display hand cards + civ

---
+++ app/scripts/controllers/board.js
@@ -0,0 +1,13 @@
+app.controller('board', function ($scope) {
+	$scope.civilisation = "babylon_b";
+	$scope.hand = [
+		"press",
+		"tree_farm",
+		"west_trading_post",
+		"glassworks",
+		"pawnshop",
+		"east_trading_post",
+		"theater",
+		"academy"
+	];
+});
